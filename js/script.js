3b2821c919c0bb56d5b45d2bea831fb69cb65607
TimmyTang22 <timmytang9822@gmail.com>
2021-04-21 01:41:07 +0800

    update 21/4/2021

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 //script
+function navActive(){
+  var nav = document.getElementsByClassName('nav')[0];
+  if(window.location.hash == "#header"){
+    nav.classList.remove('nav-active');
+  }
+  else{
+    nav.classList.add('nav-active');
+  }
+
+}
