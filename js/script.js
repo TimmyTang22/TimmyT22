d17dc0171d92ea2b00a601e842ad4decfe95d20f
TimmyTang22 <timmytang9822@gmail.com>
2021-04-22 00:35:44 +0800

    update 22/4/2021

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
-//script
-function navActive(){
-  var nav = document.getElementsByClassName('nav')[0];
-  if(window.location.hash == "#header"){
-    nav.classList.remove('nav-active');
-  }
-  else{
-    nav.classList.add('nav-active');
-  }
-
+function hashAnim(){
+    if(window.location.hash =="#home" ){
+      $('.header').addClass("header-active");
+      $('.home-anim').addClass("home-anim-active");
+      $('.human-anim').addClass("human-anim-active");
+    }
+    else {
+      $('.header').removeClass("header-active");
+      $('.home-anim').removeClass("home-anim-active");
+      $('.human-anim').removeClass("human-anim-active");
+    }
+    if(window.location.hash =="#about_me" ){
+      $('.chat-box').addClass("chat-box-active");
+    }
+    else {
+        $('.chat-box').removeClass("chat-box-active");
+    }
 }
